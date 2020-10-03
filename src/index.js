50d6b8ca3faf2bede79d07c567ab2ca0f0df4626
zebnitskyMax <58050667+zebnitskyMax@users.noreply.github.com>
2020-10-03 23:59:06 +0300

    feat: added solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 exports.min = function min(array) {
-        return array.sort((a, b) => a - b)[0] ;
+       let resultMin =  Math.min.apply(null, array) 
+  return resultMin;
 }
 
 exports.max = function max(array) {
-       return Math.max.apply(null, array) ;
+  let resultMax= Math.max.apply(null, array)
+       return resultMax ;
 }
 
 exports.avg = function avg(array) {
     let avenger = array.reduce((sum, current) => (sum + current), 0);
-  return avenger/(array.length-1);
+  return avenger/(array.length);
 }
