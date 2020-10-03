45c83d01193dc9375016c76a25215afcb3d130e1
zebnitskyMax <58050667+zebnitskyMax@users.noreply.github.com>
2020-10-03 23:40:27 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+        return array.sort((a, b) => a - b)[0] ;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+       return Math.max.apply(null, array) ;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let avenger = array.reduce((sum, current) => (sum + current), 0);
+  return avenger/(array.length-1);
 }
